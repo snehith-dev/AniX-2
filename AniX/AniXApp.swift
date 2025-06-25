6fb596732b3baec99563148eb25dc1c9c4489f10
snehith-dev <snehith0269@gmail.com>
2025-06-25 20:58:45 +0530

    Initial Commit

---
+++ AniX/AniXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniXApp.swift
+//  AniX
+//
+//  Created by Snehith Kothakota on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AniXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AniXTests/AniXTests.swift
@@ -0,0 +1,17 @@
+//
+//  AniXTests.swift
+//  AniXTests
+//
+//  Created by Snehith Kothakota on 25/06/25.
+//
+
+import Testing
+@testable import AniX
+
+struct AniXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
